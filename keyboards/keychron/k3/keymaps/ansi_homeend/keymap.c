b1d4cb79fc1c9bebaa4dc06318cb66445d598ff1
Jun Liu <jiulun@hotmail.com>
2025-02-23 22:19:42 +1100

    Index 55 is key L. Try 46.

--- keyboards/keychron/k3/keymaps/ansi_homeend/keymap.c
+++ keyboards/keychron/k3/keymaps/ansi_homeend/keymap.c
@@ -164,7 +164,7 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   if (host_keyboard_led_state().caps_lock) {
-      RGB_MATRIX_INDICATOR_SET_COLOR(55, 255, 0, 0);
+      RGB_MATRIX_INDICATOR_SET_COLOR(46, 255, 0, 0);
   }
 }
 
